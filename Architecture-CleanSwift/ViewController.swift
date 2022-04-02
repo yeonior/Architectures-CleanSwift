c39d639b1de76b08d5ddced611efdc894d4a8ccd
Rus <de.mr44@gmail.com>
2022-04-02 18:20:56 +0500

    Initial Commit

---
+++ Architecture-CleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Architecture-CleanSwift
+//
+//  Created by Ruslan on 02.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

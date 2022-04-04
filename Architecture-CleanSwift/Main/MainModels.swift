81e23d06ede732abe565af8371685a7cf3643df3
Rus <de.mr44@gmail.com>
2022-04-04 15:45:16 +0500

    Updated MainModels

--- Architecture-CleanSwift/Main/MainModels.swift
+++ Architecture-CleanSwift/Main/MainModels.swift
@@ -5,23 +5,53 @@
 //  Created by Ruslan on 03.04.2022.
 //
 
-typealias MainRequest = Main.ShowPhotoInfo.Request
-typealias MainResponse = Main.ShowPhotoInfo.Response
-typealias MainViewModel = Main.ShowPhotoInfo.ViewModel
+import Foundation
+
+protocol CellIdentifiable {
+    var cellIdentifier: String { get }
+    var cellHeight: Double { get }
+}
+
+typealias MainRequest = Main.ShowPhotos.Request
+typealias MainResponse = Main.ShowPhotos.Response
+typealias MainViewModel = Main.ShowPhotos.ViewModel
+typealias MainCellViewModel = Main.ShowPhotos.Response.CellViewModel
 
 enum Main {
     
     // use cases for main scene
-    enum ShowPhotoInfo {
+    enum ShowPhotos {
         
         // viewController -> interactor
         struct Request {
-            
+            // TEMPORARILY
+            let photos: [Photo]
         }
         
         // interactor -> presenter
         struct Response {
             
+            // cell
+            struct CellViewModel: CellIdentifiable {
+                let title: String
+                let id: Int
+                
+                var cellIdentifier: String {
+                    "cell"
+                }
+                
+                var cellHeight: Double {
+                    50
+                }
+                
+                init(photo: Photo) {
+                    title = photo.title
+                    id = photo.id
+                }
+            }
+            
+            // row of cells
+            let row: [CellIdentifiable]
         }
         
         // presenter -> viewController
